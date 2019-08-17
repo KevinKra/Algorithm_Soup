2ce205ef48c5158892a925f6c1473f84aa88d22e
Kevin Krato <kratok08@gmail.com>
2019-08-17 13:55:02 -0600

    Write a linear search function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let primesArray = [2, 3, 5, 7, 11, 13, 17];
 
+//binary search -- worst case the value is not present in the array
 const binarySearch = function(array, targetValue) {
   let min = 0;
   let max = array.length - 1;
@@ -13,6 +14,16 @@ const binarySearch = function(array, targetValue) {
       min = guess + 1;
     } else {
       max = guess - 1;
+    }
+  }
+  return -1;
+};
+
+//linear search -- worst case the value is not present in the array
+const linearSearch = function(array, targetValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === targetValue) {
+      return i;
     }
   }
   return -1;
