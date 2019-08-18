f36d6931219f2dbb50300b47156e08f2463c3e0e
Kevin Krato <kratok08@gmail.com>
2019-08-18 15:54:24 -0600

    Add multiple solutions

--- challenges.js
+++ challenges.js
@@ -36,11 +36,97 @@ var moveZeros = function(arr) {
 
 //
 var moveZeros = function(arr) {
-  var filtedList = arr.filter(function(num) {
+  var filteredList = arr.filter(function(num) {
     return num !== 0;
   });
   var zeroList = arr.filter(function(num) {
     return num === 0;
   });
-  return filtedList.concat(zeroList);
+  return filteredList.concat(zeroList);
 };
+
+//=== 2.) Narcissistic value, each number is raised by the length of the total number and added together.
+// my solution
+function narcissistic(value) {
+  //split value
+  const values = value
+    .toString()
+    .split("")
+    .map(Number);
+  //raise each value by the length of array
+  //add the values together
+  const solution = values
+    .map(value => Math.pow(value, values.length))
+    .reduce((total, curr) => {
+      total += curr;
+      return total;
+    }, 0);
+  //compare added values to input value
+  if (value === solution) return true;
+  else return false;
+}
+
+//=== 3.) First non-repeating character
+// my solution
+const firstNonRepeatingLetter = s => {
+  const index = s
+    .toLowerCase()
+    .split("")
+    .findIndex((char, _, s) => s.indexOf(char) === s.lastIndexOf(char));
+
+  return index === -1 ? "" : s[index];
+};
+
+// regex
+function firstNonRepeatingLetter(s) {
+  for (var i in s) {
+    if (s.match(new RegExp(s[i], "gi")).length === 1) {
+      return s[i];
+    }
+  }
+  return "";
+}
+
+//=== 4.) Human Readable Time
+//my solution
+function humanReadable(seconds) {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor((seconds % 3600) % 60);
+
+  const array = [h, m, s];
+
+  const formatted = array.map(function(value) {
+    if (value.toString().length < 2) {
+      return "0" + value.toString();
+    } else {
+      return value;
+    }
+  });
+
+  return `${formatted[0]}:${formatted[1]}:${formatted[2]}`;
+}
+
+//map joining
+function humanReadable(seconds) {
+  return [seconds / 3600, (seconds % 3600) / 60, seconds % 60]
+    .map(function(v) {
+      v = Math.floor(v).toString();
+      return v.length == 1 ? "0" + v : v;
+    })
+    .join(":");
+}
+
+//adv closure solution
+function humanReadable(seconds) {
+  var pad = function(x) {
+    return x < 10 ? "0" + x : x;
+  };
+  return (
+    pad(parseInt(seconds / (60 * 60))) +
+    ":" +
+    pad(parseInt((seconds / 60) % 60)) +
+    ":" +
+    pad(seconds % 60)
+  );
+}
